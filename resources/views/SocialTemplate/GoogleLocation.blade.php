7e510de07a5edde9a1a2604592cea1a2ef13f833
AleksandarStoilov89 <aleksandar.stoilov89@gmail.com>
2017-02-25 15:04:54 +0200

    add new template

---
+++ resources/views/SocialTemplate/GoogleLocation.blade.php
@@ -0,0 +1,9 @@
+<!-- Google Location -->
+<div class="w3-card-2 w3-round w3-white w3-hide-small">
+    <div class="w3-container">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1478.9915436840217!2d24.719870162704478!3d42.15064873180329!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x39219f821ba47951!2z0KHQv9C-0YDRgtC10L0g0LrQu9GD0LEg0JvQtdCz0LjQvtC90Yot0LzRg9Cw0Lkg0YLQsNC5LNC60LjQutCx0L7QutGBLNC80LzQsA!5e0!3m2!1sen!2sbg!4v1488026048405"
+                height="450" frameborder="0" style="border:0" allowfullscreen>
+        </iframe>
+    </div>
+</div>
+<br>
---
+++ resources/views/SocialTemplate/facebook.blade.php
@@ -0,0 +1,12 @@
+<!-- facebook -->
+<div class="w3-card-2 w3-round w3-white w3-hide-small">
+    <div class="w3-container">
+        <div class="fb-page" data-href="https://www.facebook.com/SportsClubLegion/" data-tabs="timeline"
+             data-small-header="false"
+             data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+            <blockquote cite="https://www.facebook.com/hiGifs/" class="fb-xfbml-parse-ignore"><a
+                        href="https://www.facebook.com/hiGifs/">Higifs.com</a></blockquote>
+        </div>
+    </div>
+</div>
+<br>
