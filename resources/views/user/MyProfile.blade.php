f5cbefa76367cce27d6ad07444f5e64c48ce07c3
AleksandarStoilov89 <aleksandar.stoilov89@gmail.com>
2017-02-26 22:57:51 +0200

    new user profile

--- resources/views/user/MyProfile.blade.php
+++ resources/views/user/MyProfile.blade.php
@@ -1,125 +1,168 @@
 @extends('layouts.app')
 
 @section('content')
+    {{--    <img src="{{ URL::asset('public/img/asd.jpg') }}" style="width:45%;" class="w3-round"><br><br>--}}
 
     <!-- Navbar -->
     @include('Navbar.Navbar')
     <!-- Navbar End -->
-    <!-- Sidenav/menu -->
-    <br/>
-    <nav class="w3-sidenav w3-collapse w3-white w3-animate-left" style="z-index:3;width:300px;" id="mySidenav"><br>
-        <div class="w3-container">
-            <a href="#" onclick="w3_close()" class="w3-hide-large w3-right w3-jumbo w3-padding" title="close menu">
-                <i class="fa fa-remove"></i>
-            </a>
-            <img src="{{ URL::asset('public/img/asd.jpg') }}" style="width:45%;" class="w3-round"><br><br>
-            <h4 class="w3-padding-0"><b>PORTFOLIO</b></h4>
-            <p class="w3-text-grey">Template by W3.CSS</p>
-        </div>
-        <a href="#portfolio" onclick="w3_close()" class="w3-padding w3-text-teal"><i
-                    class="fa fa-th-large fa-fw w3-margin-right"></i>PORTFOLIO</a>
-        <a href="#about" onclick="w3_close()" class="w3-padding"><i
-                    class="fa fa-user fa-fw w3-margin-right"></i>ABOUT</a>
-        <a href="#contact" onclick="w3_close()" class="w3-padding"><i class="fa fa-envelope fa-fw w3-margin-right"></i>CONTACT</a>
+    </br>
+    </br>
+    </br>
 
-        <div class="w3-section w3-padding-top w3-large">
-            <a href="#" class="w3-hover-white w3-hover-text-indigo w3-show-inline-block"><i
-                        class="fa fa-facebook-official"></i></a>
-            <a href="#" class="w3-hover-white w3-hover-text-purple w3-show-inline-block"><i class="fa fa-instagram"></i></a>
-            <a href="#" class="w3-hover-white w3-hover-text-yellow w3-show-inline-block"><i class="fa fa-snapchat"></i></a>
-            <a href="#" class="w3-hover-white w3-hover-text-red w3-show-inline-block"><i class="fa fa-pinterest-p"></i></a>
-            <a href="#" class="w3-hover-white w3-hover-text-light-blue w3-show-inline-block"><i
-                        class="fa fa-twitter"></i></a>
-            <a href="#" class="w3-hover-white w3-hover-text-indigo w3-show-inline-block"><i class="fa fa-linkedin"></i></a>
+    <!-- Side Navigation -->
+    <nav class="w3-sidenav w3-collapse w3-white w3-animate-left w3-card-2" style="z-index:3;width:320px;"
+         id="mySidenav">
+        <a href="javascript:void(0)" class="w3-border-bottom w3-large"><img
+                    src="https://www.w3schools.com/images/w3schools.png" style="width:60%;"></a>
+        <a href="javascript:void(0)" onclick="w3_close()" title="Close Sidemenu"
+           class="w3-hide-large w3-closenav w3-large">Close <i class="fa fa-remove"></i></a>
+        <a href="javascript:void(0)" class="w3-dark-grey w3-button w3-hover-black w3-left-align"
+           onclick="document.getElementById('id01').style.display='block'">New Message <i
+                    class="w3-padding-left fa fa-pencil"></i></a>
+        <div>
+            <a id="myBtn" onclick="myFunc('Demo1')" href="javascript:void(0)"><i
+                        class="fa fa-inbox w3-padding-right"></i>Inbox (3)<i
+                        class="w3-padding-left fa fa-caret-down"></i></a>
+            <div id="Demo1" class="w3-hide w3-animate-left">
+                <a href="javascript:void(0)" class="w3-border-bottom test w3-hover-light-grey"
+                   onclick="openMail('Borge');w3_close();" id="firstTab">
+                    <div class="w3-container">
+                        <img class="w3-round w3-margin-right" src="/w3images/avatar3.png" style="width:15%;"><span
+                                class="w3-opacity w3-large">Borge Refsnes</span>
+                        <h6>Subject: Remember Me</h6>
+                        <p>Hello, i just wanted to let you know that i'll be home at...</p>
+                    </div>
+                </a>
+                <a href="javascript:void(0)" class="w3-border-bottom test w3-hover-light-grey"
+                   onclick="openMail('Jane');w3_close();">
+                    <div class="w3-container">
+                        <img class="w3-round w3-margin-right" src="/w3images/avatar5.png" style="width:15%;"><span
+                                class="w3-opacity w3-large">Jane Doe</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+                    </div>
+                </a>
+                <a href="javascript:void(0)" class="w3-border-bottom test w3-hover-light-grey"
+                   onclick="openMail('John');w3_close();">
+                    <div class="w3-container">
+                        <img class="w3-round w3-margin-right" src="/w3images/avatar2.png" style="width:15%;"><span
+                                class="w3-opacity w3-large">John Doe</span>
+                        <p>Welcome!</p>
+                    </div>
+                </a>
+            </div>
         </div>
+        <a href="javascript:void(0)"><i class="fa fa-paper-plane w3-padding-right"></i>Sent</a>
+        <a href="javascript:void(0)"><i class="fa fa-hourglass-end w3-padding-right"></i>Drafts</a>
+        <a href="javascript:void(0)"><i class="fa fa-trash w3-padding-right"></i>Trash</a>
     </nav>
 
-    <!-- Overlay effect when opening sidenav on small screens -->
-    <div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer"
-         title="close side menu" id="myOverlay"></div>
-
-    <!-- !PAGE CONTENT! -->
-    <div class="w3-main" style="margin-left:300px">
-
-        <!-- Header -->
-        <header class="w3-container" id="portfolio">
-            <a href="#"><img src="{{ URL::asset('public/img/asd.jpg') }}" style="width:65px;"
-                             class="w3-circle w3-right w3-margin w3-hide-large w3-hover-opacity"></a>
-            <span class="w3-opennav w3-hide-large w3-xxlarge w3-hover-text-grey" onclick="w3_open()"><i
-                        class="fa fa-bars"></i></span>
-            <h1><b>My Portfolio</b></h1>
-
-        </header>
-
-        <!-- First Photo Grid-->
-        <div class="w3-row-padding">
-            <div class="w3-third w3-container w3-margin-bottom">
-                <img src="{{ URL::asset('public/img/asd.jpg') }}" alt="Norway" style="width:100%"
-                     class="w3-hover-opacity">
+    <!-- Modal that pops up when you click on "New Message" -->
+    <div id="id01" class="w3-modal" style="z-index:4">
+        <div class="w3-modal-content w3-animate-zoom">
+            <div class="w3-container w3-padding w3-red">
+                <span onclick="document.getElementById('id01').style.display='none'"
+                      class="w3-right w3-xxlarge w3-closebtn"><i class="fa fa-remove"></i></span>
+                <h2>Send Mail</h2>
             </div>
-            <div class="w3-third w3-container w3-margin-bottom">
-                <img src="{{ URL::asset('public/img/asd.jpg') }}" alt="Norway" style="width:100%"
-                     class="w3-hover-opacity">
+            <div class="w3-panel">
+               <form action="#" method="post" >
+                   <label>To</label>
+                   <input class="w3-input w3-border w3-margin-bottom" type="text">
+                   <label>From</label>
+                   <input class="w3-input w3-border w3-margin-bottom" type="text">
+                   <label>Subject</label>
+                   <input class="w3-input w3-border w3-margin-bottom" type="text">
+                   <input class="w3-input w3-border w3-margin-bottom" style="height:150px"
+                          placeholder="What's on your mind?">
+                   <div class="w3-section">
+                       <a class="w3-button w3-red" onclick="document.getElementById('id01').style.display='none'">Cancel
+                            <i class="fa fa-remove"></i></a>
+                       <input type="submit" name="zapazi" class="w3-button w3-right">
+                   </div>
+               </form>
             </div>
-            <div class="w3-third w3-container w3-margin-bottom">
-                <img src="{{ URL::asset('public/img/asd.jpg') }}" alt="Norway" style="width:100%"
-                     class="w3-hover-opacity">
-            </div>
-            <div class="w3-third w3-container w3-margin-bottom">
-                <img src="{{ URL::asset('public/img/asd.jpg') }}" alt="Norway" style="width:100%"
-                     class="w3-hover-opacity">
-            </div>
-            <div class="w3-third w3-container w3-margin-bottom">
-                <img src="{{ URL::asset('public/img/asd.jpg') }}" alt="Norway" style="width:100%"
-                     class="w3-hover-opacity">
-            </div>
-            <div class="w3-third w3-container w3-margin-bottom">
-                <img src="{{ URL::asset('public/img/asd.jpg') }}" alt="Norway" style="width:100%"
-                     class="w3-hover-opacity">
-            </div>
-            <div class="w3-third w3-container w3-margin-bottom">
-                <img src="{{ URL::asset('public/img/asd.jpg') }}" alt="Norway" style="width:100%"
-                     class="w3-hover-opacity">
-            </div>
-            <div class="w3-third w3-container w3-margin-bottom">
-                <img src="{{ URL::asset('public/img/asd.jpg') }}" alt="Norway" style="width:100%"
-                     class="w3-hover-opacity">
-            </div>
-            <div class="w3-third w3-container w3-margin-bottom">
-                <img src="{{ URL::asset('public/img/asd.jpg') }}" alt="Norway" style="width:100%"
-                     class="w3-hover-opacity">
-            </div>
-        </div>
-
-
-        <div class="w3-container w3-padding-large" style="margin-bottom:32px">
-            <h4><b>About Me</b></h4>
-            <p>Just me, myself and I, exploring the universe of unknownment. I have a heart of love and an interest of
-                lorem ipsum and mauris neque quam blog. I want to share my world with you. Praesent tincidunt sed tellus
-                ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.
-                Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies
-                congue gravida diam non fringilla.</p>
-            <hr>
-            {{--action="{{ action('UserController@logUser') }}"--}}
-            <form action="form.asp" target="_blank" enctype="multipart/form-data">
-                <div class="w3-group">
-                    <label>Name</label>
-                    <input class="w3-input w3-border" type="text" name="Name" required>
-                </div>
-                <div class="w3-group">
-                    <label>Email</label>
-                    <input class="w3-input w3-border" type="text" name="Email" required>
-                </div>
-                <div class="w3-group">
-                    <label>Message</label>
-                    <input class="w3-input w3-border" type="text" name="Message" required>
-                </div>
-                <button type="submit" class="w3-btn w3-padding-large w3-margin-bottom"><i
-                            class="fa fa-paper-plane w3-margin-right"></i>Send Message
-                </button>
-            </form>
         </div>
     </div>
+
+    <!-- Overlay effect when opening the side navigation on small screens -->
+    <div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer"
+         title="Close Sidemenu" id="myOverlay"></div>
+
+    <!-- Page content -->
+    <div class="w3-main" style="margin-left:320px;">
+        <i class="fa fa-bars w3-opennav w3-hide-large w3-xlarge w3-margin-left w3-margin-top" onclick="w3_open()"></i>
+        <a href="javascript:void(0)" class="w3-hide-large w3-red w3-button w3-right w3-margin-top w3-margin-right"
+           onclick="document.getElementById('id01').style.display='block'"><i class="fa fa-pencil"></i></a>
+
+        <div id="Borge" class="w3-container person">
+            <br>
+            <img class="w3-round  w3-animate-top" src="{{ URL::asset('public/img/asd.jpg') }}" style="width:20%;">
+            <h5 class="w3-opacity">Subject: Remember Me</h5>
+            <h4><i class="fa fa-clock-o"></i> From Borge Refsnes, Sep 27, 2015.</h4>
+            <a class="w3-button" href="#">Reply<i class="w3-padding-left fa fa-mail-reply"></i></a>
+            <a class="w3-button" href="#">Forward<i class="w3-padding-left fa fa-arrow-right"></i></a>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                ut aliquip ex ea commodo consequat.</p>
+            <p>Best Regards, <br>Borge Refsnes</p>
+        </div>
+
+
+    </div>
+
+    </div>
+
+    <script>
+        var openInbox = document.getElementById("myBtn");
+        openInbox.click();
+
+        function w3_open() {
+            document.getElementById("mySidenav").style.display = "block";
+            document.getElementById("myOverlay").style.display = "block";
+        }
+        function w3_close() {
+            document.getElementById("mySidenav").style.display = "none";
+            document.getElementById("myOverlay").style.display = "none";
+        }
+
+        function myFunc(id) {
+            var x = document.getElementById(id);
+            if (x.className.indexOf("w3-show") == -1) {
+                x.className += " w3-show";
+                x.previousElementSibling.className += " w3-red";
+            } else {
+                x.className = x.className.replace(" w3-show", "");
+                x.previousElementSibling.className =
+                        x.previousElementSibling.className.replace(" w3-red", "");
+            }
+        }
+
+        openMail("Borge")
+        function openMail(personName) {
+            var i;
+            var x = document.getElementsByClassName("person");
+            for (i = 0; i < x.length; i++) {
+                x[i].style.display = "none";
+            }
+            x = document.getElementsByClassName("test");
+            for (i = 0; i < x.length; i++) {
+                x[i].className = x[i].className.replace(" w3-light-grey", "");
+            }
+            document.getElementById(personName).style.display = "block";
+            event.currentTarget.className += " w3-light-grey";
+        }
+    </script>
+
+    <script>
+        var openTab = document.getElementById("firstTab");
+        openTab.click();
+    </script>
+
 
     @include('layouts.Footer')
 
