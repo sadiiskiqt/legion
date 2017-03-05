b0fbd3485a7f06f6a57cf7bea7166f5703c89e6d
AleksandarStoilov89 <aleksandar.stoilov89@gmail.com>
2017-03-05 22:41:41 +0200

    mobile menu

--- resources/views/LegionData.blade.php
+++ resources/views/LegionData.blade.php
@@ -201,5 +201,30 @@ Read more at: https://www.w3schools.com/graphics/google_maps_basic.asp
 <footer class="w3-container w3-padding-32 w3-theme-d1 w3-center">
     <h4>СК "Легион" - Пловдив / SC "Legion" - Plovdiv,</h4>
 </footer>
+
+<script>
+    // Accordion
+    function myFunction(id) {
+        var x = document.getElementById(id);
+        if (x.className.indexOf("w3-show") == -1) {
+            x.className += " w3-show";
+            x.previousElementSibling.className += " w3-theme-d1";
+        } else {
+            x.className = x.className.replace("w3-show", "");
+            x.previousElementSibling.className =
+                x.previousElementSibling.className.replace(" w3-theme-d1", "");
+        }
+    }
+
+    // Used to toggle the menu on smaller screens when clicking on the menu button
+    function openNav() {
+        var x = document.getElementById("navDemo");
+        if (x.className.indexOf("w3-show") == -1) {
+            x.className += " w3-show";
+        } else {
+            x.className = x.className.replace(" w3-show", "");
+        }
+    }
+</script>
 </body>
 </html>
--- resources/views/Navbar/Navbar.blade.php
+++ resources/views/Navbar/Navbar.blade.php
@@ -3,12 +3,15 @@
     <ul class="w3-navbar w3-theme-d2 w3-left-align w3-large">
         <li class="w3-hide-medium w3-hide-large w3-opennav w3-right">
             <a class="w3-padding-large w3-hover-white w3-large w3-theme-d2" href="javascript:void(0);"
-               onclick="openNav()"><i class="fa fa-bars"></i></a>
+               onclick="openNav()">
+                <i class="fa fa-bars"></i>
+            </a>
         </li>
         <li>
             <a href="legion" class="w3-padding-large w3-theme-d4">
                 <i class="fa fa-home w3-margin-right"></i>mmaLegion.com</a>
         </li>
+
 
         @if (Auth::guest())
             <li class="w3-hide-small">
@@ -17,9 +20,9 @@
                 </a>
             </li>
             {{--<li class="w3-hide-small">--}}
-                {{--<a href="fbgallery" class="w3-padding-large w3-hover-white" title="News">--}}
-                    {{--FB Галерия--}}
-                {{--</a>--}}
+            {{--<a href="fbgallery" class="w3-padding-large w3-hover-white" title="News">--}}
+            {{--FB Галерия--}}
+            {{--</a>--}}
             {{--</li>--}}
             <li class="w3-hide-small">
                 <a href="home" class="w3-padding-large w3-hover-white" title="News">
@@ -36,9 +39,9 @@
                 </a>
             </li>
             {{--<li class="w3-hide-small">--}}
-                {{--<a href="fbgallery" class="w3-padding-large w3-hover-white" title="News">--}}
-                    {{--FB Галерия--}}
-                {{--</a>--}}
+            {{--<a href="fbgallery" class="w3-padding-large w3-hover-white" title="News">--}}
+            {{--FB Галерия--}}
+            {{--</a>--}}
             {{--</li>--}}
             <li class="w3-hide-small">
                 <a href="home" class="w3-padding-large w3-hover-white" title="Нови публикации">
@@ -47,30 +50,30 @@
                     </i>
                 </a>
             </li>
-            {{--<li class="w3-hide-small">--}}
-                {{--<a href="#" class="w3-padding-large w3-hover-white" title="Account Settings">--}}
-                    {{--<i class="fa fa-user">--}}
-                    {{--</i>--}}
-                {{--</a>--}}
-            {{--</li>--}}
-            {{--<li class="w3-hide-small">--}}
-                {{--<a href="#" class="w3-padding-large w3-hover-white" title="Messages">--}}
-                    {{--<i class="fa fa-envelope">--}}
-                    {{--</i>--}}
-                {{--</a>--}}
-            {{--</li>--}}
-            {{--<li class="w3-hide-small w3-dropdown-hover">--}}
-                {{--<a href="#" class="w3-padding-large w3-hover-white" title="Notifications">--}}
-                    {{--<i class="fa fa-bell">--}}
-                    {{--</i>--}}
-                    {{--<span class="w3-badge w3-right w3-small w3-green">3</span>--}}
-                {{--</a>--}}
-                {{--<div class="w3-dropdown-content w3-white w3-card-4">--}}
-                    {{--<a href="#">One new friend request</a>--}}
-                    {{--<a href="#">John Doe posted on your wall</a>--}}
-                    {{--<a href="#">Jane likes your post</a>--}}
-                {{--</div>--}}
-            {{--</li>--}}
+            <li class="w3-hide-small">
+                <a href="#" class="w3-padding-large w3-hover-white" title="Account Settings">
+                    <i class="fa fa-user">
+                    </i>
+                </a>
+            </li>
+            <li class="w3-hide-small">
+                <a href="#" class="w3-padding-large w3-hover-white" title="Messages">
+                    <i class="fa fa-envelope">
+                    </i>
+                </a>
+            </li>
+            <li class="w3-hide-small w3-dropdown-hover">
+                <a href="#" class="w3-padding-large w3-hover-white" title="Notifications">
+                    <i class="fa fa-bell">
+                    </i>
+                    <span class="w3-badge w3-right w3-small w3-green">3</span>
+                </a>
+                <div class="w3-dropdown-content w3-white w3-card-4">
+                    <a href="#">One new friend request</a>
+                    <a href="#">John Doe posted on your wall</a>
+                    <a href="#">Jane likes your post</a>
+                </div>
+            </li>
 
             <li class="w3-hide-small w3-right">
                 <a href="{{ route('logout') }}"
@@ -84,7 +87,8 @@
             </li>
             <li class="w3-hide-small w3-right">
                 <a href="#" class="w3-padding-large w3-hover-white" title="My Account">
-                    <img src="{{ URL::asset('public/img/asd.jpg') }}" class="w3-circle" style="height:25px;width:25px" alt="Avatar">
+                    <img src="{{ URL::asset('public/img/asd.jpg') }}" class="w3-circle" style="height:25px;width:25px"
+                         alt="Avatar">
                 </a>
             </li>
             <li class="w3-hide-small w3-right">
@@ -99,9 +103,16 @@
 <!-- Navbar on small screens -->
 <div id="navDemo" class="w3-hide w3-hide-large w3-hide-medium w3-top" style="margin-top:51px">
     <ul class="w3-navbar w3-left-align w3-large w3-theme">
-        <li><a class="w3-padding-large" href="#">Link 1</a></li>
-        <li><a class="w3-padding-large" href="#">Link 2</a></li>
-        <li><a class="w3-padding-large" href="#">Link 3</a></li>
-        <li><a class="w3-padding-large" href="#">My Profile</a></li>
+        <li><a class="w3-padding-large" href="legion">За Нас</a></li>
+        <li><a class="w3-padding-large" href="home">Нови Постове</a></li>
+        @if (!Auth::guest())
+            <li><a class="w3-padding-large" href="myprofile">Моя Профил</a></li>
+            <li><a class="w3-padding-large" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                   document.getElementById('logout-form').submit();">
+                    Изход
+                </a>
+            </li>
+        @endif
     </ul>
 </div>
